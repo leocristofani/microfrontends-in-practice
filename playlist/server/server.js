1f23a3fc20a0b02f6705d27735f939c689a65a70
Leo Cristofani <leo.cristofani@gmail.com>
2018-08-23 23:51:14 -0300

    create playlist microfrontend

---
+++ playlist/server/server.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const path = require('path');
+const cors = require('cors');
+const express = require('express');
+
+const PORT = 3005;
+
+const app = express();
+
+/**
+ * When embedded as a micro-frontend the frontend will do cross origin requests.
+ */
+app.use(cors());
+
+app.use('/public', express.static(`${__dirname}/public`));
+
+function getPathToEmbedAssets() {
+  const filesPath = {};
+  const embedPath = 'public/embed';
+  fs.readdirSync(`${__dirname}/${embedPath}`)
+    .forEach(fileName => {
+      if (fileName.indexOf('.css') !== -1) {
+        filesPath['css'] = `${embedPath}/${fileName}`;
+      }
+      if (fileName.indexOf('.js') !== -1) {
+        filesPath['js'] = `${embedPath}/${fileName}`;
+      }
+    });
+
+  return filesPath;
+}
+
+/**
+ * Exposes paths to embed assets
+ */
+app.get('/api/embed-assets', (req, res) => {
+  res.json(getPathToEmbedAssets());
+});
+
+app.get('*', (req, res) => {
+  const template = path.resolve(__dirname, 'public/index.html');
+  res.sendFile(template);
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
